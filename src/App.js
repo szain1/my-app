a6b46200c531e915c7de75068ab6ccbcc1d0b1db
szain1 <talktozainn@gmail.com>
2025-05-08 21:47:58 +0500

    w

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './componets/Navbar';
+import TextForm from './componets/TextForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+<Navbar  title="Home" aboutus="Get Start"/>
+<div className="cointainer">
+<TextForm heading=" To Capitalize" />
+</div>
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    </>
+   
+
+
   );
 }
 
 export default App;
+
+
+
---
+++ src/componets/TextForm.js
@@ -0,0 +1,48 @@
+import React,{use, useState} from 'react'
+// import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
+    const handleClick = ()=> {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handlelowClick = ()=> {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleonchange = (event)=>{
+        setText(event.target.value)
+    }
+    const[text, setText] =useState("Enter your text")
+  return (
+<>
+    <div className="container my-3">
+    <h1>{props.heading}</h1>
+    <div className="mb-3">
+      <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleonchange}></textarea>
+    </div>
+    <button className="btn btn-primary mx-2" onClick={handleClick}>Convert to the uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handlelowClick}>Convert to thelowercase</button>
+    
+    <button className="btn btn-primary mx-2" onClick={()=>{setText("empaty")}}>Clear Text</button>
+    <button className="btn btn-primary mx-2" onClick={()=>{navigator.clipboard.writeText(text); alert("text copied")} }>Copy Text</button>
+    {/* <button className="btn btn-primary mx-2" onClick={()=>{alert("text copied")}}>past</button>   */}
+    
+  </div>
+<div className="container my-3">
+    <h2>your text summery</h2>
+    <p> {text.length} Character  , {text.split(" ").length} Words</p>
+    <p> {0.008 * text.split(" ").length} Minutes to read</p>
+    <h2>Preview</h2>
+    <p>{text.length>0? text: "Enter something to preview it here"}</p>
+</div>
+
+
+
+  </>
+
+
+  
+  )
+}
