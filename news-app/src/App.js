5bbfca2bc70f11d00fd4fe860ed0dc7822092603
szain1 <talktozainn@gmail.com>
2025-05-15 23:21:11 +0500

    Remove unused logo import and clean up whitespace in App component

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-import React from 'react'
 
 import React, { Component } from 'react'
 
@@ -9,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        
+        hello
       </div>
     )
   }
