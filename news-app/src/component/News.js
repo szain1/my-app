cedf21d35d70ba7563db26a3ebe2d4514f90aa44
szain1 <talktozainn@gmail.com>
2025-05-16 18:58:58 +0500

    Add headlines and multiple NewsItems instances to News component for enhanced display

--- news-app/src/component/News.js
+++ news-app/src/component/News.js
@@ -5,12 +5,41 @@ export class News extends Component {
   render() {
     return (
       <div>
+        <h1>News monkey top headlines</h1>
 
-        <NewsItems/>
-         <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-       
+
+        <div className="row">
+          <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+        </div>
+
+
+         <div className="row">
+          <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+           <div className="col md-4">
+            <NewsItems title="My title" discribtion="My discribtion"/>
+          </div>
+        </div>
+
+      
        
         
       </div>
