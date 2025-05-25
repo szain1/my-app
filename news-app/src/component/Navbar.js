b58406e8108e40d5a6e591a34abadc70697c782a
szain1 <talktozainn@gmail.com>
2025-05-26 00:35:15 +0500

    Update Navbar component to change link text from 'Link' to 'TEXT'

--- news-app/src/component/Navbar.js
+++ news-app/src/component/Navbar.js
@@ -15,7 +15,7 @@ export class Navbar extends Component {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">TEXT</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
