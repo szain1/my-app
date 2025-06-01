18b7275cfa2ba913e1f3abfc1b5c5f24022d122b
szain1 <talktozainn@gmail.com>
2025-06-01 17:43:31 +0500

    Fix Navbar component structure and improve accessibility

--- news-app/src/component/Navbar.js
+++ news-app/src/component/Navbar.js
@@ -12,7 +12,7 @@ export class Navbar extends Component {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">home</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">TEXT</a>
